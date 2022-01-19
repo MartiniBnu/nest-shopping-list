9b3ee0899b4eef5070cf554309ec6ecb5625a341
Marcos Martini <martini.bnu@gmail.com>
2022-01-19 00:33:15 -0300

    Everything is ok, we've created all the methods and the swagger

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,1 +1,20 @@
-export class CreateItemDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateItemDto {
+  @ApiProperty({example: 'Bananas'})
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiPropertyOptional({example: 'Silver Bananas', description: 'Optional description of the item'})
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @ApiProperty({example: 5, description: 'Needed quantity'})
+  @IsInt()
+  @Min(0)
+  quantity: number;
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Shopping list API')
+    .setDescription('My shopping list API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.SERVER_PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
